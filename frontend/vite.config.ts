d55e8141472c627c311be88e79f78790b89260d0
Girum <70520544+girume1@users.noreply.github.com>
2026-03-05 23:14:17 +0300

    vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,31 @@
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-assetsInclude: ['**/*.wasm'],
-    worker: {
-        format: "es",
+  assetsInclude: ["**/*.wasm"],
+  worker: {
+    format: "es",
+  },
+  plugins: [
+    react(),
+    mkcert(),
+  ],
+  build: {
+    target: "esnext",
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    exclude: ["@provablehq/wasm"],
+  },
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), "../sdk"],
     },
-    plugins: [react()],
-    build: {
-        target: "esnext",
-        sourcemap: true,
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
-    optimizeDeps: {
-        exclude: ["@provablehq/wasm"],
-    },
-    server: {
-        fs: {
-            allow: [searchForWorkspaceRoot(process.cwd()), "../sdk"],
-        },
-        headers: {
-            "Cross-Origin-Opener-Policy": "same-origin",
-            "Cross-Origin-Embedder-Policy": "require-corp",
-        },
-    },
+  },
 });
