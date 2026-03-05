0b48306b96c9ebd63fb8398448c926379c5e48fc
Girum <70520544+girume1@users.noreply.github.com>
2026-03-05 23:57:29 +0300

    vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,37 +1,27 @@
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const isDev = process.env.NODE_ENV !== "production";
-
-export default defineConfig(async () => {
-  const plugins: any[] = [react()];
-
-  if (isDev) {
-    const mkcert = (await import("vite-plugin-mkcert")).default;
-    plugins.push(mkcert());
-  }
-
-  return {
-    assetsInclude: ["**/*.wasm"],
-    worker: {
-      format: "es",
+// https://vitejs.dev/config/
+export default defineConfig({
+  assetsInclude: ["**/*.wasm"],
+  worker: {
+    format: "es",
+  },
+  plugins: [react()], 
+  build: {
+    target: "esnext",
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    exclude: ["@provablehq/wasm"],
+  },
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), "../sdk"],
     },
-    plugins,
-    build: {
-      target: "esnext",
-      sourcemap: true,
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
-    optimizeDeps: {
-      exclude: ["@provablehq/wasm"],
-    },
-    server: {
-      fs: {
-        allow: [searchForWorkspaceRoot(process.cwd()), "../sdk"],
-      },
-      headers: {
-        "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Embedder-Policy": "require-corp",
-      },
-    },
-  };
+  },
 });
