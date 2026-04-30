9e6ae1344243a7ed4487363fe717e5bd67305730
Girum <70520544+girume1@users.noreply.github.com>
2026-04-30 21:19:27 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -54,7 +54,7 @@ export default function App() {
       wallets={wallets}
       network={Network.TESTNET}
       decryptPermission={DecryptPermission.UponRequest}
-      autoConnect={true}
+      autoConnect={}
       programs={[PROGRAM_ID, CREDITS_PROGRAM, USDCX_PROGRAM]}
     >
       <WalletModalProvider>
